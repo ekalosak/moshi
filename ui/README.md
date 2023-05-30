f453e6e427213f4b28713a4ca0249217f7832ac2
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-05-30 15:23:01 -0400

    hello world flask app

---
+++ ui/README.md
@@ -0,0 +1,8 @@
+# ui
+The browser-based user interface for Moshi.
+
+# Usage
+Run the web server with:
+```sh
+flask --app ui.main run
+```
---
+++ ui/ui/__init__.py
@@ -0,0 +1,7 @@
+""" This module provides a browser-based user interface for Moshi.
+It handles streaming audio from the user's mic to the server and from the server to the user's speakers.
+This package is intended to be invoked as a web server from the commandline. It is unusual to, for example, import this
+package for use in other Python projects.
+
+In short, this is a Flask app - see https://flask.palletsprojects.com/en/2.3.x/ for more information.
+"""

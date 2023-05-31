c7396e17d0b0de44cb6be95a19400cc799d38400
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-05-30 20:08:22 -0400

    stream connection works, the JS cycles through the stream (as known by the server/main.py stdout showing the chunks printed successfully); but I can't get the stream result to render on the frontend...

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,27 @@
 import random
+import string
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, stream_with_context
 
 app = Flask(__name__)
+
+def random_string(length):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+def generate_chunks(length, num_chunks):
+    for i in range(num_chunks):
+        yield random_string(length)
+
+@app.route('/stream', methods=['GET'])
+def stream():
+    @stream_with_context
+    def generate():
+        for chunk in generate_chunks(10, 10):
+            print(chunk)
+            yield chunk
+
+    response = Response(generate(), mimetype='text/event-stream')
+    return response
 
 @app.route("/")
 def index():

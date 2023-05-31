6d612e76bd9714452ad7e918410cb7c82b886222
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-05-30 22:08:48 -0400

    logs streaming to frontend

--- server/CHANGELOG.md
+++ server/CHANGELOG.md
@@ -1,2 +1,5 @@
+# 23.5.1 WIP
+Logging streamed to frontend
+
 # 23.5.0
 Basic implementation of text streaming from server to client using websockets.
--- server/server/main.py
+++ server/server/main.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from pprint import pprint
 import random
 import string
 import time
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from loguru import logger
+
+import moshi
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 socketio = SocketIO(app)
 
+class WebSocketHandler:
+    # Custom logger to emit log messages through WebSocket
+    def __call__(self, record):
+        log_data = {
+            'level': record.record['level'].name,
+            'message': record.record['message'],
+            'time': record.record['time'].isoformat(),
+            'name': record.record['name'],
+            'function': record.record['function'],
+            'line': record.record['line'],
+        }
+        socketio.emit('log', log_data)
+
+log_handler = WebSocketHandler()
+logger.add(log_handler)
+
 def random_string(length):
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
 
-def generate_chunks(length, num_chunks=None):
-    if num_chunks:
-        for i in range(num_chunks):
-            yield random_string(length)
-    else:
-        while 1:
-            yield random_string(length)
+def generate_chunks(length, num_chunks):
+    for i in range(num_chunks):
+        yield random_string(length)
 
 @socketio.on('connect')
 def handle_connect():
@@ -31,8 +48,9 @@ def handle_disconnect():
 @socketio.on('start_stream')
 def handle_start_stream():
     print('Starting stream')
-    for chunk in generate_chunks(10):
+    for chunk in generate_chunks(10, 10):
         print(chunk)
+        logger.info('some ping')
         time.sleep(random.uniform(0., 1.))
         emit('stream_data', {'data': chunk})
 

b95150c2138d640955ff610f485df70df4426c7a
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-02 14:15:07 -0400

    wip

--- server/server/rtc.py
+++ server/server/rtc.py
@@ -2,100 +2,47 @@
 import argparse
 import asyncio
 import json
-import logging
 import os
 import ssl
 import uuid
 
-import cv2
+from pprint import pformat
+import functools
+import sys
+
 from aiohttp import web
 from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
 
-ROOT = os.path.dirname(__file__)
+from loguru import logger
+from loguru._defaults import LOGURU_FORMAT
 
-logger = logging.getLogger("pc")
-pcs = set()
+ROOT = os.path.dirname(__file__)
+LOG_FORMAT = LOGURU_FORMAT + " | <g><d>{extra}</d></g>"
+logger.remove()
+logger.add(sink=sys.stderr, format=LOG_FORMAT, colorize=True)
+
+pcs = set()  # peer connections
 relay = MediaRelay()
 
-class AudioTrack(MediaStreamTrack):
+class WebMicStream(MediaStreamTrack):
     """
-    An audio stream track that provides access to frames from an another track.
-    This adapts the WebRTC MediaStreamTrack to an AudioSource..? for sr.
+    An audio stream track that provides access to frames from a WebRTC track.
     """
-    ...
+    kind = 'audio'
 
-# class VideoTransformTrack(MediaStreamTrack):
-#     """
-#     A video stream track that transforms frames from an another track.
-#     """
-#
-#     kind = "video"
-#
-#     def __init__(self, track, transform):
-#         super().__init__()  # don't forget this!
-#         self.track = track
-#         self.transform = transform
-#
-#     async def recv(self):
-#         frame = await self.track.recv()
-#
-#         if self.transform == "cartoon":
-#             img = frame.to_ndarray(format="bgr24")
-#
-#             # prepare color
-#             img_color = cv2.pyrDown(cv2.pyrDown(img))
-#             for _ in range(6):
-#                 img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-#             img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-#
-#             # prepare edges
-#             img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#             img_edges = cv2.adaptiveThreshold(
-#                 cv2.medianBlur(img_edges, 7),
-#                 255,
-#                 cv2.ADAPTIVE_THRESH_MEAN_C,
-#                 cv2.THRESH_BINARY,
-#                 9,
-#                 2,
-#             )
-#             img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-#
-#             # combine color and edges
-#             img = cv2.bitwise_and(img_color, img_edges)
-#
-#             # rebuild a VideoFrame, preserving timing information
-#             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-#             new_frame.pts = frame.pts
-#             new_frame.time_base = frame.time_base
-#             return new_frame
-#         elif self.transform == "edges":
-#             # perform edge detection
-#             img = frame.to_ndarray(format="bgr24")
-#             img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-#
-#             # rebuild a VideoFrame, preserving timing information
-#             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-#             new_frame.pts = frame.pts
-#             new_frame.time_base = frame.time_base
-#             return new_frame
-#         elif self.transform == "rotate":
-#             # rotate image
-#             img = frame.to_ndarray(format="bgr24")
-#             rows, cols, _ = img.shape
-#             M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-#             img = cv2.warpAffine(img, M, (cols, rows))
-#
-#             # rebuild a VideoFrame, preserving timing information
-#             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-#             new_frame.pts = frame.pts
-#             new_frame.time_base = frame.time_base
-#             return new_frame
-#         else:
-#             return frame
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+
+    async def recv(self):
+        frame = await self.track.recv()
+        # TODO put this into the AudioSource..?
+        return frame
 
 
 async def index(request):
+    logger.info(request)
     content = open(os.path.join(ROOT, "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
 
@@ -104,26 +51,31 @@ async def javascript(request):
     content = open(os.path.join(ROOT, "client.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
 
+def async_with_pcid(f):
+    @functools.wraps(f)
+    async def wrapped(*a, **k):
+        pcid = uuid.uuid4()
+        # with logger.contextualize(extra={'PeerConnection': str(pcid)}):
+        with logger.contextualize(PeerConnection=str(pcid)):
+            return await f(*a, **k)
+    return wrapped
 
+@async_with_pcid
 async def offer(request):
+    """ In WebRTC, there's an initial offer->answer exchange that negotiates the SDP (sess. desc. protoc.).
+    This endpoint handles an offer request from a client and returns an answer with the SDP 
+    """
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
     pc = RTCPeerConnection()
-    pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
 
-    def log_info(msg, *args):
-        logger.info(pc_id + " " + msg, *args)
-
-    log_info("Created for %s", request.remote)
+    logger.info(f"Created for: {request.remote}")
 
     # prepare local media
-    player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
-    if args.record_to:
-        recorder = MediaRecorder(args.record_to)
-    else:
-        recorder = MediaBlackhole()
+    # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+    recorder = MediaBlackhole()  # TODO this should be an AudioSream that provides AudioSource to speech_recognition
 
     @pc.on("datachannel")
     def on_datachannel(channel):
@@ -134,24 +86,24 @@ async def offer(request):
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
-        log_info("Connection state is %s", pc.connectionState)
+        logger.info(f"Connection state is: {pc.connectionState}")
         if pc.connectionState == "failed":
             await pc.close()
             pcs.discard(pc)
 
     @pc.on("track")
     def on_track(track):
-        log_info("Track %s received", track.kind)
+        logger.info(f"Track {track.kind} received")
 
         if track.kind == "audio":
-            pc.addTrack(player.audio)
+            # pc.addTrack(player.audio)  # NOTE this plays the wav; it should be instead a player that relays the synth voice
             recorder.addTrack(track)
         else:
             raise TypeError(f"Track kind not supported, expected 'audio', got: '{track.kind}'")
 
         @track.on("ended")
         async def on_ended():
-            log_info("Track %s ended", track.kind)
+            logger.info(f"Track {track.kind} ended")
             await recorder.stop()
 
     # handle offer
@@ -162,6 +114,7 @@ async def offer(request):
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
+    logger.debug(f"answer: {answer}")
     return web.Response(
         content_type="application/json",
         text=json.dumps(
@@ -189,14 +142,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--port", type=int, default=5000, help="Port for HTTP server (default: 5000)"
     )
-    parser.add_argument("--record-to", help="Write received media to a file."),
-    parser.add_argument("--verbose", "-v", action="count")
+    # parser.add_argument("--verbose", "-v", action="count")
     args = parser.parse_args()
-
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
 
     if args.cert_file:
         ssl_context = ssl.SSLContext()

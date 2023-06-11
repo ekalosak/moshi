dbbc7786ec724b02749a72670c290b1b6da1e4d7
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-10 21:38:14 -0400

    removed duplicated wav writer

--- server/tests/test_detector.py
+++ server/tests/test_detector.py
@@ -4,18 +4,6 @@ import pytest
 
 from server import audio
 
-def write_frame(frame: AudioFrame) -> str:
-    fn = 'test_utterance.wav'
-    container = av.open(fn, 'w')
-    try:
-        stream = container.add_stream('pcm_s16le', layout='mono')
-        stream.rate = 44100
-        container.mux(stream.encode(frame))
-    finally:
-        container.close()
-    return fn
-
-
 @pytest.mark.asyncio
 @pytest.mark.slow
 async def test_utterance_detector(utterance_audio_track):
@@ -30,8 +18,6 @@ async def test_utterance_detector(utterance_audio_track):
     print('got_utterance! stopping...')
     await ud.stop()
     print('stopped!')
-    # fn = write_frame(utterance)
-    # print(f"wrote detected utterance to: {fn}")
     assert isinstance(utterance, AudioFrame)
     utterance_time = audio.get_frame_seconds(utterance)
     assert 8. <= utterance_time <= 9., f"{str(utterance_audio_track)} is nominally 8.56 seconds of speech"

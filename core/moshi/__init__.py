b2e6428817fdfa8cb2c09bb688fdbd18b852fff1
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-12 18:23:04 -0400

    renamed server/ to moshi/; started migrating useful source files from core/moshi to moshi/

--- core/moshi/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import Message, Role, Model, ModelType
-# # from .lang import Language
-# from .chat import Chatter, CliChatter
---
+++ moshi/__init__.py
@@ -0,0 +1,2 @@
+from .base import Message, Model, ModelType, Role
+from .chat import WebRTCChatter

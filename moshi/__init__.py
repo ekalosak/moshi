7004aedc8e7f5239c441c9a8a44efc1e046b1e8f
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-12 18:37:32 -0400

    merged server/__init__.py to __init__.py

--- moshi/__init__.py
+++ moshi/__init__.py
@@ -1,4 +1,10 @@
+import os
 from .base import Message, Model, ModelType, Role
 from .chat import WebRTCChatter
 from .detector import UtteranceDetector
 from .responder import ResponsePlayer
+
+SAMPLE_RATE = int(os.getenv("MOSHISAMPLERATE", 48000))
+AUDIO_FORMAT = os.getenv("MOSHIAUDIOFORMAT", 's16')
+AUDIO_LAYOUT = os.getenv("MOSHIAUDIOFORMAT", 'stereo')
+OPENAI_TRANSCRIPTION_MODEL = os.getenv("OPENAI_TRANSCRIPTION_MODEL", "whisper-1")

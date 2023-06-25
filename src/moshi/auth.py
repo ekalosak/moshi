0a9f44c7ee00b74cd2f9bdf184d977aaf5aae2a0
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-25 15:42:27 -0400

    limit 1 the auth user query

--- src/moshi/auth.py
+++ src/moshi/auth.py
@@ -7,7 +7,7 @@ db = firestore.AsyncClient(project=GOOGLE_PROJECT)
 users_ref = db.collection("authorized_users")
 
 async def is_email_authorized(email: str) -> bool:
-    query = users_ref.where("email", "==", email)
+    query = users_ref.where("email", "==", email).limit(1)
     results = await query.get()
     if len(results) > 0:
         logger.info(f"User exists in authorized_users collection: {email}")

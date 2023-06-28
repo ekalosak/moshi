adf9320f6ba3438d4540aabf0eac096e6aac6633
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-06-27 20:52:16 -0400

    responded to linting

--- app/main.py
+++ app/main.py
@@ -10,8 +10,6 @@ async def app_factory():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Moshi web app")
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
     parser.add_argument(
         "--host", default="127.0.0.1", help="Host for HTTP server (default: 127.0.0.1)"
     )
@@ -19,12 +17,7 @@ if __name__ == "__main__":
         "--port", type=int, default=5000, help="Port for HTTP server (default: 5000)"
     )
     args = parser.parse_args()
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
     app = asyncio.run(app_factory())
     web.run_app(
-        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+        app, access_log=None, host=args.host, port=args.port
     )

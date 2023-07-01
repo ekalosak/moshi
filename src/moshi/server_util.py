585dd2f1e1b1f4e2c55616ed2b33b1c2494d89d6
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-01 17:40:58 -0400

    index link functional

--- src/moshi/server_util.py
+++ src/moshi/server_util.py
@@ -1,11 +1,15 @@
 from moshi import __version__
 
-def render(template, request) -> 'html':
-    privacy_url = str(request.app.router['privacy'].url_for())
+def render(template, request, **kwargs) -> 'html':
+    """Add default routes to template from request.app.router; pass kwg to template.render()"""
+    index_url = str(request.app.router['index'].url_for())
     news_url = str(request.app.router['news'].url_for())
+    privacy_url = str(request.app.router['privacy'].url_for())
     html = template.render(
         version=f"alpha-{__version__}",
-        privacy_url=privacy_url,
+        index_url=index_url,
         news_url=news_url,
+        privacy_url=privacy_url,
+        **kwargs
     )
     return html

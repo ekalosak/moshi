9655f5b43719b7523d0d1deb9e53fcf3a72e8080
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-15 17:46:13 -0400

    CORS stuff, now frontend can get health

--- src/moshi/api.py
+++ src/moshi/api.py
@@ -1,17 +1,29 @@
 # TODO make stateles api
 
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from moshi import Message, Conversation
 from moshi.auth import firebase_auth
 
 app = FastAPI()
-logger.success("Loaded!")
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+logger.warning("Using permissive CORS for development. In production, only allow requests from known origins.")
 
 @app.get("/healthz")
-def healthz():
+def healthz(request: Request):
     """Health check endpoint"""
+    from pprint import pprint
+    pprint(dict(request.headers))
+    logger.debug(f"Request headers: {request.headers}")
     return "OK"
 
 @app.get("/m/{kind}/new")
@@ -30,3 +42,5 @@ async def new_conversation(kind: str, auth: dict = Depends(firebase_auth)):
             "did": did,
         }
     }
+
+logger.success("Loaded!")

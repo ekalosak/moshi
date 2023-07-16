ab87daddb5171fcc88746b974b9fe8e0ead0256d
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-16 11:46:33 -0400

    added inits;

--- src/moshi/activities/__init__.py
+++ src/moshi/activities/__init__.py
@@ -1,4 +1,1 @@
-"""The convos module contains application logic for the conversational Moshi modules.
-Each module under `moshi.convos.kinds` supports a conversational "plugin" for the main app.
-For example, the `unstructured` module provides the logic for the POC activity.
-"""
+"""This module contains application logic for Moshi plugins."""

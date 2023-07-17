1b5e70201ed408cc76ed1c270454accb9b290aa9
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-07-17 14:16:26 -0400

    wip paired with moshi-client bb55380; responding with audio that flutter can read and headers that flutter can read

--- src/moshi/server/api.py
+++ src/moshi/server/api.py
@@ -1,12 +1,13 @@
+import json
 from pprint import pformat, pprint
 
 from fastapi import FastAPI, Depends, HTTPException, Request, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse, FileResponse
+from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
-
-from fastapi.middleware.cors import CORSMiddleware
-from loguru import logger
 
 from moshi import activities, util
 from moshi.core.storage import firestore_client
@@ -31,6 +32,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 logger.warning("Using permissive CORS for development. In production, only allow requests from known origins.")
 
@@ -86,40 +88,76 @@ async def new_conversation(kind: str, user: dict = Depends(firebase_auth)):
     }
 
 @app.post("/m/next/{cid}")
-async def user_utterance(cid: str, utterance: UploadFile, user: dict = Depends(firebase_auth)):
+async def user_utterance(
+        cid: str,
+        utterance: UploadFile,
+        user: dict = Depends(firebase_auth),
+    ):
     """Submit recorded audio to Moshi."""
+    # headers = {
+    #     "user-utterance": "user said this",
+    #     "assistant-utterance": "moshi responded with that",
+    # }
+    # def audio_iterator():
+    #     utterance.file.seek(0)
+    #     while chunk := utterance.file.read(4096):
+    #         yield chunk
+    # response = StreamingResponse(
+    #     audio_iterator(),
+    #     media_type="audio/m4a",
+    #     headers=headers,
+    # )
+    # return response
+    # # NOTE ^ for debug
+
     uid = user['uid']
-    collection_ref = firestore_client.collection("conversations")
-    doc_ref = collection_ref.document(cid)
-    doc = await doc_ref.get()
-    if not doc:
-        raise HTTPException(
-            status_code = 400,
-            detail = {"message": f"Document not found: {cid}"}
-        )
-    elif doc.get('uid') != uid:
-        raise HTTPException(
-            status_code = 400,
-            detail = {"message": f"Document belongs to different user: {doc.get('uid')}"}
-        )
-    kind = doc.get('kind')
-    if not kind:
-        raise HTTPException(
-            status_code = 500,
-            detail = {"message": f"Data format error, 'kind' not found in document."}
-        )
-    # TODO make chatter/activity
-    # TODO submit audio via activity
-    # TODO respond with transcripts and utterance
-    breakpoint()
-    logger.debug(f"Submitting audio for conversation with cid: {cid}")
-    return {
-        "message": "Audio submitted",
-        "detail" : {
-            "conversation_id": cid,
+    with logger.contextualize(cid=cid):
+        collection_ref = firestore_client.collection("conversations")
+        doc_ref = collection_ref.document(cid)
+        logger.debug(f"Getting doc from 'conversations' collection: {cid}")
+        doc = await doc_ref.get()
+        if not doc:
+            raise HTTPException(
+                status_code = 400,
+                detail = {"message": f"Document not found: {cid}"}
+            )
+        elif doc.get('uid') != uid:
+            raise HTTPException(
+                status_code = 400,
+                detail = {"message": f"Document belongs to different user: {doc.get('uid')}"}
+            )
+        kind = doc.get('kind')
+        if not kind:
+            raise HTTPException(
+                status_code = 500,
+                detail = {"message": f"Data format error, 'kind' not found in document."}
+            )
+        logger.debug(f"Activity kind: {kind}")
+        # TODO make chatter/activity
+        # TODO submit audio via activity
+        # TODO respond with transcripts and utterance
+
+        ## TODO WIP respond with the same audio to check roundtrip:
+        ## https://fastapi.tiangolo.com/advanced/custom-response/#using-streamingresponse-with-file-like-objects
+        ## TODO WIP also include dummy text in the response json (return media AND json)
+        # breakpoint()
+        # logger.debug(f"Submitting audio for conversation with cid: {cid}")
+        utterance.file.seek(0)
+        print(utterance.file.read(128))
+
+        headers = {
+            "user-utterance": "user said this",
+            "assistant-utterance": "moshi responded with that",
         }
-    }
-
-
+        def audio_iterator():
+            utterance.file.seek(0)
+            while chunk := utterance.file.read(4096):
+                yield chunk
+        response = StreamingResponse(
+            audio_iterator(),
+            media_type="audio/m4a",
+            headers=headers,
+        )
+        return response
 
 logger.success("Loaded!")

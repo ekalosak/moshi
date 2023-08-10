ea406834f6d5626d579932c54608cc7d2b5afab7
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-08-10 19:22:39 -0400

    init firestore revved for moshi-3 and including the 'info' collection (news, updates, etc)

--- scripts/init_firestore.py
+++ scripts/init_firestore.py
@@ -2,7 +2,7 @@
 - `transcripts`
 - `profiles`
 - `config`
-- `moshinews`
+- `news`
 
 First it creates the config collection:
 - document ID is `supported_langs`:
@@ -18,13 +18,14 @@ Third, it creates a default profile for the default user with the following attr
 - `primary_lang`: `en-US`
 - `name`: `Timmy Test`
 
-Fourth, it creates a doc in the moshinews collection:
+Fourth, it creates a doc in the info collection:
 - `type`: `privacy_policy`
 - `body`: `...`
 """
 
 import os
 from sys import exit
+from datetime import datetime
 
 import firebase_admin
 import google.cloud.firestore as firestore
@@ -32,7 +33,7 @@ from firebase_admin import auth
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-PROJECT_ID = "moshi-002"
+PROJECT_ID = "moshi-3"
 COLLECTIONS = ["transcripts", "profiles", "config", "moshinews"]
 DEFAULT_USER_EMAIL = "test@test.test"
 DEFAULT_USER_PASSWORD = "testtest"
@@ -83,11 +84,33 @@ def _init_config(db):
     doc_ref.set({"langs": SUPPORTED_LANGS})
     print("Successfully initialized the config collection.")
 
-def _init_moshinews(db):
+def _init_info(db):
     """Initialize the moshinews collection."""
-    doc_ref = db.collection("moshinews").document("privacy_policy")
-    doc_ref.set({"body": "..."})
-    print("Successfully initialized the moshinews collection.")
+    doc_ref = db.collection("info").document()
+    doc_ref.set({
+        "title": "Privacy Policy",
+        "subtitle": "Last updated: 2023-08-09",
+        "body": "We take your privacy seriously. What data we do store is encrypted at rest and in flight. We do not share your data with anyone but you and select 3rd party API providers. We do not sell your data. We do not currently use your data for advertising purposes, but may do so in the future in de-identified aggregate.\nWe do use your data to improve our service. We do not currently have a way for you to delete your data in the App, but we will happily do so upon request by email at moshi.feedback@gmail.com. We do not store audio recordings, but we do store transcripts of the conversations you have with Moshi. These transcripts are available to you on the Transcripts page.\nWe do not knowingly collect data from children under 13. If you are a parent or guardian and believe we have collected data from your child, please contact us at moshi.feedback@gmail.com and we will remove it immediately.\nWe may update this privacy policy at any time, please check back for updates.",
+        "type": "privacy_policy",
+        "timestamp": datetime.now(),
+        })
+    doc_ref = db.collection("info").document()
+    doc_ref.set({
+        "title": "Latest updates",
+        "subtitle": "Please check for updates regularly.",
+        "body": "Please visit www.chatmoshi.com for the latest updates to the Moshi app.",
+        "type": "update",
+        "timestamp": datetime.now(),
+    })
+    doc_ref = db.collection("info").document()
+    doc_ref.set({
+        "title": "Moshi Beta is live!",
+        "subtitle": "Thank you for giving Moshi a try!",
+        "body": "The Moshi Beta is now live! Thank you for your patience as we continue to improve the service. Please reach out with any suggestions or feedback via the Feedback page.",
+        "type": "news",
+        "timestamp": datetime.now(),
+    })
+    print("Successfully initialized the info collection.")
 
 def _init_profile(db, uid):
     """Initialize the profiles collection."""
@@ -112,7 +135,7 @@ def _init_user(auth):
         user = auth.get_user_by_email(DEFAULT_USER_EMAIL)
     else:
         print('Successfully created new user.')
-    print("Default user: ", user.uid, user.email, user.display_name)
+    print("Default user: ",  user.email, user.display_name)
     return user.uid
 
 def main():
@@ -122,7 +145,7 @@ def main():
     db = _init_firestore()
     _init_profile(db, uid)
     _init_config(db)
-    _init_moshinews(db)
+    _init_info(db)
 
 if __name__ == "__main__":
     print("START")
